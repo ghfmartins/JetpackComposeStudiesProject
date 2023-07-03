7866256e5bbd00473e2773d0b95a72bb10f936f7
Gustavo Martins <gh.martins9@gmail.com>
2023-07-03 20:53:35 -0300

    Studying rows and columns

--- app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
+++ app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
@@ -1,40 +1,60 @@
 /*
     Created by: Gustavo Martins
-    Practicing video: ~
+    Practicing video: Rows, Columns & Basic Sizing - Android Jetpack Compose - Part 2 at <https://youtu.be/rHKeRWK3zL4>
  */
 package com.ghfmartins.jetpackcomposestudiesproject
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.ghfmartins.jetpackcomposestudiesproject.ui.theme.JetpackComposeStudiesProjectTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Gustavo")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Text("Hello")
+                Text("World")
+                Row(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .fillMaxHeight(0.5f)
+                        .background(Color.Yellow),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Text("How")
+                    Text("are")
+                    Text("you?")
+                }
+            }
+
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Greeting("Gustavo")
+
 }
