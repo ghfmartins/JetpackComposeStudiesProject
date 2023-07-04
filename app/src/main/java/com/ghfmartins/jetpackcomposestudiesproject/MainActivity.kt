9e3c1139f77eccbf3d0fddabee8bed50ed1657bb
Gustavo Martins <gh.martins9@gmail.com>
2023-07-03 21:31:31 -0300

    Studying modifiers on jetpack compose

--- app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
+++ app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
@@ -1,40 +1,65 @@
 /*
     Created by: Gustavo Martins
-    Practicing video: ~
+    Practicing video: Modifiers - Android Jetpack Compose - Part 3 at <https://youtu.be/XCuC_p3E0qo>
  */
 package com.ghfmartins.jetpackcomposestudiesproject
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.ghfmartins.jetpackcomposestudiesproject.ui.theme.JetpackComposeStudiesProjectTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Gustavo")
+          Column(
+              modifier = Modifier
+                  .fillMaxHeight(0.5f)
+                  .fillMaxWidth()
+//                  .requiredWidth(600.dp)
+                  .border(50.dp, Color.Green)
+                  .padding(50.dp)
+                  .border(40.dp, Color.Yellow)
+                  .padding(40.dp)
+                  .background(Color.Blue),
+              verticalArrangement = Arrangement.Center
+
+          ){
+              Row(
+                  verticalAlignment = Alignment.CenterVertically,
+                  horizontalArrangement = Arrangement.SpaceEvenly,
+                  modifier = Modifier
+                      .fillMaxWidth()
+                      .background(Color.White)
+              ){
+                  Text("Ordem")
+                  Text("e")
+                  Text("Progresso")
+              }
+          }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Greeting("Gustavo")
+
 }
