8bb65c3f5a1c5c39abb5cbc34bf53901ef81755f
Gustavo Martins <gh.martins9@gmail.com>
2023-07-03 21:41:30 -0300

    Update MainActivity.kt

--- app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
+++ app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
@@ -9,12 +9,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -48,8 +52,11 @@ class MainActivity : ComponentActivity() {
                       .background(Color.White)
               ){
                   Text("Ordem")
+                  Spacer(modifier = Modifier.width(20.dp))
                   Text("e")
-                  Text("Progresso")
+                  Text("Progresso", modifier = Modifier.clickable {
+
+                  })
               }
           }
         }
