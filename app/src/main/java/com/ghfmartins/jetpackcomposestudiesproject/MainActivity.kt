1052bdcddd0affacf5da18c901d28d6c5ce88e75
Gustavo Martins <gh.martins9@gmail.com>
2023-07-09 19:15:00 -0300

    Creating an Image Card.

--- app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
+++ app/src/main/java/com/ghfmartins/jetpackcomposestudiesproject/MainActivity.kt
@@ -1,40 +1,98 @@
 /*
     Created by: Gustavo Martins
-    Practicing video: ~
+    Practicing video: Creating an Image Card Composable - Android Jetpack Compose at <https://youtu.be/KPVoQjwmWX4>
  */
 package com.ghfmartins.jetpackcomposestudiesproject
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ghfmartins.jetpackcomposestudiesproject.ui.theme.JetpackComposeStudiesProjectTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Gustavo")
+            val painter = painterResource(id = R.drawable.mustang_on_sunset)
+            val description = "A black shelby mustang gt500 on the sunset"
+            val title = "Black Shelby Mustang GT500"
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
+                )
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
         modifier = modifier
-    )
-}
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.elevatedCardElevation(5.dp)
+    ) {
+        Box(modifier = modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Text(
+                text = title,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(12.dp),
+                fontSize = 16.sp,
+                color = Color.White,
+//                style = TextStyle(color = Color.White, fontSize = 16.sp) //Another way to style the text
+            )
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Gustavo")
 }
